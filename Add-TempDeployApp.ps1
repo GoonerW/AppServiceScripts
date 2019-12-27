2b1c2d69992e6f4ac4b677277c35e19c6d2b9cdd
GoonerW <31711960+GoonerW@users.noreply.github.com>
2019-12-27 12:26:40 +1000

    Update Add-TempDeployApp.ps1
    Now uses Get-AzureRMWebApp(Slot) and Set-AzureRmWebApp(Slot) to add temporary Virtual Directory to existing configuration

--- Add-TempDeployApp.ps1
+++ Add-TempDeployApp.ps1
@@ -1,8 +1,4 @@
-## IMPORTANT
-## This script currently makes assumptions about the target app service.  Running this blindly may result in loss of virtual directory configuration
-## The assumption is that before running this script, there is a single virtual application "/" pointing to "site\wwwroot\docroot"
-## Always verify these scripts in a testing environment before using in a production capacity
-
+# Now uses Get-AzureRMWebApp(Slot) and Set-AzureRmWebApp(Slot) to add Virtual Directory to existing configuration
 
 #Retrieve variables from environment and bail if not set
 
@@ -23,24 +19,29 @@ $websiteName = "$Env:WEBSITENAME"
 $slotName = if ($env:SLOTNAME) { $env:SLOTNAME } else { "NoSlot" } 
 $WebAppApiVersion = "2018-02-01"
 
-Function SetWebAppConfig($ResourceGroupName, $websiteName, $slotName, $ConfigObject)
-{
-    if($slotName -eq "NoSlot" )
-    {
-        write-Host "Configuring root web app"
-        Set-AzureRmResource -ResourceGroupName $ResourceGroupName -ResourceType Microsoft.Web/sites/Config -Name $websiteName/web -PropertyObject $ConfigObject -ApiVersion $WebAppApiVersion -Force
-    }
-    else
-    {
-        write-Host "Configuring deployment slot: $slotName"
-        Set-AzureRmResource -ResourceGroupName $ResourceGroupName -ResourceType Microsoft.Web/sites/slots/Config -Name $websiteName/$slotName/web -PropertyObject $ConfigObject -ApiVersion $WebAppApiVersion -Force
-    }    
-}
 Write-Host "Set a virtual application"
-$props=@{
-    virtualApplications = @(
-        @{ virtualPath = "/"; physicalPath = "site\wwwroot\docroot" },
-        @{ virtualPath = "/tempdeploy"; physicalPath = "site\wwwroot" }
-    )
+
+# Retrieve Web App
+$website = Get-AzureRmWebApp -Name $env:WEBSITENAME -ResourceGroupName $env:RESOURCEGROUPNAME
+
+# Create new Virtual Application object and configure path
+$VirtualApp = New-Object Microsoft.Azure.Management.WebSites.Models.VirtualApplication
+$VirtualApp.VirtualPath = "/tempdeploy" 
+$VirtualApp.PhysicalPath = "site\wwwroot" 
+
+if($slotName -eq "NoSlot" )
+{
+    write-Host "Configuring root web app"
+    
+    $website.SiteConfig.VirtualApplications.Add($virtualApp);
+    $website | Set-AzureRmWebApp
+    
 }
-SetWebAppConfig $ResourceGroupName $websiteName $slotName $props
+else
+{
+    write-Host "Configuring deployment slot: $slotName"
+    #Retrieve slot from main web app
+    $slotsite = Get-AzureRmWebAppSlot -WebApp $website -Slot $slotName
+    $slotsite.SiteConfig.VirtualApplications.Add($virtualApp);
+    $slotsite | Set-AzureRmWebAppSlot
+}    
